c77a6bcffdcabac6976cb0968bb216668f07dd7b
sufia10 <93113636+sufiii19@users.noreply.github.com>
2022-02-01 10:29:58 +0000

    p6 done

---
+++ p6original.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+  void input_two_string(char *a, char *b)
+  {
+    printf("Enter a word: ");
+    scanf("%s", a);
+    printf("Enter a word: ");
+    scanf("%s", b);
+  }
+
+  int cmp(char *a, char *b)
+  {
+    int i=0, results;
+    while(a[i] != '\0' && b[i] != '\0' )
+    {
+      if(a[i] > b[i])
+      {
+        results= -1;
+        break;
+      }
+      else if(b[i] > a[i])
+      {
+        results= 1;
+        break;
+      } else
+      {
+        i++;
+      }
+    }
+    if(a[i] == '\0')
+    {
+      results= 1;
+    }
+    else if(b[i] == '\0')
+    {
+      results= -1;
+    }
+    if(a[i] == '\0' && b[i] == '\0')
+    {
+      results=0;
+    }
+    return results;
+  }
+
+  void output(char *a, char *b, int ans)
+  {
+      if(ans == -1)
+      {
+        printf("%s is greater than %s\n", a, b);
+      }
+      else if(ans == 1)
+      {
+        printf("%s is greater than %s\n", b, a);
+      }
+      else
+      {
+        printf("%s is equal to %s\n", a, b);
+      }
+  }
+
+  int main()
+  {
+    char a[20], b[20];
+    int ans;
+    input_two_string(a, b);
+    ans = cmp(a, b);
+    output(a, b, ans);
+    return 0;
+  }
+
